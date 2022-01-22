f014bdb80c15e276284df8efa91727d7610d1952
= <anguyen031@gmail.com>
2022-01-22 15:55:25 -0800

    Add previously created landing page template

---
+++ templates/landing-page.php
@@ -0,0 +1,70 @@
+<!-- function to use console.log in PHP -->
+<?php function console_log( $dataToLog ){
+  echo '<script>';
+  echo 'console.log('. json_encode( $dataToLog ) .')';
+  echo '</script>';
+}
+?>
+
+<?php
+/**
+ * Template Name: Landing Page */
+get_header();
+
+?>
+<main id="landingPageContent" role="main">
+<?php
+
+	if ( have_posts() ) {
+
+		while ( have_posts() ) {
+			the_post();
+
+			get_template_part( 'template-parts/content', get_post_type() );
+		}
+	}
+	?>
+
+<?php 
+  $basePrice = get_field("middling_price");
+  if (get_field("price_multiplier_mini")) {
+    $miniPrice = (get_field("price_multiplier_mini")) * $basePrice;
+  } else {
+    $miniPrice = $basePrice;
+  }
+  if (get_field("price_multiplier_massive")) {
+    $massivePrice = (get_field("price_multiplier_massive")) * $basePrice;
+  } else {
+    $massivePrice = $basePrice;
+  }
+?>
+
+<?php
+	$topic = get_field("page_image_header");
+?>
+
+<section class="cardHolder">
+	<div class="card">
+  <img src="<?php echo content_url();?>/uploads/2022/01/<?php echo $topic ?>1.jpg')" class="cardImg" alt="">
+  <div class="container">
+    <p><b>Mini</b></p>
+    <p>Price: $<?php echo number_format($miniPrice, 2, '.', ''); ?></p>
+  </div>
+	</div>
+	<div class="card">
+  <img src="<?php echo content_url();?>/uploads/2022/01/<?php echo $topic ?>2.jpg')" class="cardImg" alt="">
+  <div class="container">
+  <p><b>Middle</b></p>
+    <p>Price: $<?php echo number_format($basePrice, 2, '.', ''); ?></p>
+  </div>
+	</div>
+	<div class="card">
+  <img src="<?php echo content_url();?>/uploads/2022/01/<?php echo $topic ?>3.jpg" class="cardImg" alt="">
+  <div class="container">
+  <p><b>Massive</b></p>
+    <p>Price: $<?php echo number_format($massivePrice, 2, '.', ''); ?></p>
+  </div>
+	</div>
+</section>
+
+<?php get_footer(); ?>
