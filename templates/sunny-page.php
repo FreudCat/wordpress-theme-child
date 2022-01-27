bf9fca47e27f911c0dfbce02928e92c0fab7ef43
= <anguyen031@gmail.com>
2022-01-26 19:34:00 -0800

    Update font colors

--- templates/sunny-page.php
+++ templates/sunny-page.php
@@ -45,7 +45,7 @@ if (wp_style_is( 'bootstrapCSS', $list )) {
       <div class="col-lg-7 mx-auto text-holder">
         <p class="cube-header order-sm-first">Transform your brand</p>
         <p class="cube-text">We are a full-service creative agency specializing in helping brands grow fast. Engage your clients through compelling visuals that do most of the marketing for you.</p>
-        <a class="cube-link transform-div" href="#">LEARN MORE</a>
+        <a class="cube-link transform-div" href="#"><span>LEARN MORE</a>
       </div>
     </div>
   </div>
@@ -55,22 +55,27 @@ if (wp_style_is( 'bootstrapCSS', $list )) {
       <img src='<?php echo get_stylesheet_directory_uri();?>/assets/images/sunny-page-images/stand-out.jpg'>
     </div>
     <div class="col-sm align-self-center">
-      <div class="col-lg-7 mx-auto text-holder">
-        <p class="cube-header text-center">Stand out to the right audience</p>
-        <p class="cube-text text-center">Using a collaborative formula of designer, researcher, photographers, videographers, and copywriters, we will build and extend you brand in digital places.</p>
+      <div class="col-lg-8 mx-auto text-holder">
+        <p class="cube-header">Stand out to the right audience</p>
+        <p class="cube-text">Using a collaborative formula of designer, researcher, photographers, videographers, and copywriters, we will build and extend you brand in digital places.</p>
+        <a class="cube-link stand-out-div" href="#"><span>LEARN MORE</a>
       </div>
     </div>
   </div>
 
   <div class="row sunny-height gx-0">
     <div class="col-sm main-page-image-div" style="background-image: url('<?php echo get_stylesheet_directory_uri();?>/assets/images/sunny-page-images/design.jpg');">  
-      <p class="graphic-design-header">Graphic Design</p>
-      <p>Great design makes you memorable. We deliver artwork that underscores your brand message and captures potential client's attention.</p>
+    <div class="col-lg-7 mx-auto design-holder">
+      <p class="graphic-design-header text-center">Graphic Design</p>
+      <p class="graphic-design-text text-center">Great design makes you memorable. We deliver artwork that underscores your brand message and captures potential client's attention.</p>
+      </div>
     </div>
     <div class="col-sm main-page-image-div" style="background-image: url('<?php echo get_stylesheet_directory_uri();?>/assets/images/sunny-page-images/photography.jpg');">
-      <p class="photography-header">Photography</p>
-      <p>Increase your credibility by getting the most stunning, high-quality photos that improve your business image.</p>
+    <div class="col-lg-6 mx-auto design-holder">  
+    <p class="photography-header text-center">Photography</p>
+      <p class="photography-text text-center">Increase your credibility by getting the most stunning, high-quality photos that improve your business image.</p>
     </div>
+</div>
   </div>
 
   <div class="testimonial-header row gx-0 text-center">
