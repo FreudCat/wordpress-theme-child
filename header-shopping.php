fb995baccffed21b2b7236f7f87af4b20029ba5a
= <anguyen031@gmail.com>
2022-01-31 19:13:33 -0800

    Avatar border

--- header-shopping.php
+++ header-shopping.php
@@ -22,13 +22,25 @@
 </head>
 <body>
   <header>
-    <nav class="navbar navbar-expand-lg shopping-nav navbar-light bg-light ">
-      <div class="container-fluid shopping-nav">
+    <nav class="navbar navbar-expand-lg  navbar-light shopping-nav">
+      <div class="container-fluid shopping-nav-container gx-0">
         <div class="d-inline-flex align-items-center">
           <button class="navbar-toggler shopping-nav-toggle" type="button" data-bs-toggle="offcanvas" data-bs-target="#shoppingSupportedContent" aria-controls="shoppingSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <i class="fas fa-bars"></i>
           </button>
           <a class="navbar-brand shopping-logo" href="#"><img src="<?php echo get_stylesheet_directory_uri();?>/assets/images/shopping-page-images/logo.svg"></a>
+          <div class="collapse navbar-collapse">
+          <ul class='navbar-nav flex-nowrap'>
+          <!-- These are expanded on the left in desktop view-->
+              <li class='nav-item'><a class = "nav-link offcanvas-link" href="#">Collections</a></li> <!--greyish text - class="shopping-header-items", class="shopping-header-links"-->
+              <li class='nav-item'><a class = "nav-link offcanvas-link" href="#">Men</a></li><!--on hover and active - border bottom is orange and thick, text becomes black-->
+              <li class='nav-item'><a class = "nav-link offcanvas-link" href="#">Women</a></li>
+              <li class='nav-item'><a class = "nav-link offcanvas-link" href="#">About</a></li>
+              <li class='nav-item'><a class = "nav-link offcanvas-link" href="#">Contact</a></li>
+            <!-- These are in a column in a sidebar in mobile view -->
+            <!-- These are on the right and remain in both views-->
+            </ul>
+        </div>
         </div>
         <div class="offcanvas shopping offcanvas-start" tabindex="-1" id="shoppingSupportedContent" aria-labelledby="offcanvasExampleLabel">
           <div class="offcanvas-header">
@@ -37,20 +49,22 @@
           <div class="offcanvas-body">
             <ul class='navbar-nav flex-nowrap shopping-unordered-list'>
           <!-- These are expanded on the left in desktop view-->
-              <li class='nav-item shoe-header-item'><a class = "nav-link" id="offcanvas-link" href="#">Collections</a></li> <!--greyish text - class="shopping-header-items", class="shopping-header-links"-->
-              <li class='nav-item shoe-header-item'><a class = "nav-link" id="offcanvas-link" href="#">Men</a></li><!--on hover and active - border bottom is orange and thick, text becomes black-->
-              <li class='nav-item shoe-header-item'><a class = "nav-link" id="offcanvas-link" href="#">Women</a></li>
-              <li class='nav-item shoe-header-item'><a class = "nav-link" id="offcanvas-link" href="#">About</a></li>
-              <li class='nav-item shoe-header-item'><a class = "nav-link" id="offcanvas-link" href="#">Contact</a></li>
+              <li class='nav-item'><a class = "nav-link offcanvas-link" href="#">Collections</a></li> <!--greyish text - class="shopping-header-items", class="shopping-header-links"-->
+              <li class='nav-item'><a class = "nav-link offcanvas-link" href="#">Men</a></li><!--on hover and active - border bottom is orange and thick, text becomes black-->
+              <li class='nav-item'><a class = "nav-link offcanvas-link" href="#">Women</a></li>
+              <li class='nav-item'><a class = "nav-link offcanvas-link" href="#">About</a></li>
+              <li class='nav-item'><a class = "nav-link offcanvas-link" href="#">Contact</a></li>
             <!-- These are in a column in a sidebar in mobile view -->
             <!-- These are on the right and remain in both views-->
             </ul>
           </div>
         </div>
-        <div class="d-inline-flex">
+        <div class="d-inline-flex align-items-center">
           <a class ='nav-link ms-auto p-2' href="#"><img class="cart-image" src="<?php echo get_stylesheet_directory_uri();?>/assets/images/shopping-page-images/icon-cart.svg"></a> <!--cart with items ->oval/orange icon with white number in the center-->
           <!-- <cartmodal>rectangle with rounded borders and box shadow on desktop --- on mobile, covers move of the shoe image, also has box shadow  --> 
-          <img class="avatar" src="<?php echo get_stylesheet_directory_uri();?>/assets/images/shopping-page-images/image-avatar.png"> <!--round; on hover and active - orange border; class="avatar"-->
+          <div class="border">
+            <img class="avatar rounded-circle p-0" src="<?php echo get_stylesheet_directory_uri();?>/assets/images/shopping-page-images/image-avatar.png"> 
+            </div>
         </div>
       </div>
     </nav> 
