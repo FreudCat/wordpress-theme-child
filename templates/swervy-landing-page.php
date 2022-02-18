b554dd9ef1b56217e924b00710a4ae6df5480f31
= <anguyen031@gmail.com>
2022-02-17 20:55:22 -0800

    Start swervy-landing page

---
+++ templates/swervy-landing-page.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Template Name: Swervy Landing Page */
+remove_action('wp_head', '_admin_bar_bump_cb');
+?>
+
+<main class="swervy-main">
+  <header>
+    <div class="swervy-logo">1</div>
+    <div class="try-button">2</div>
+  </header>
+ 
+  <div>3</div>
+  <div>4</div>
+  <div>5</div>
+  <div>6</div>
+  <div>7</div>
+  <div>8</div>
+  <div>9</div>
+  <div>10</div>
+</main>
