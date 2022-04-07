0e329c2a087b154b5cdfea8aefb4eb841fcb0293
= <anguyen031@gmail.com>
2022-04-06 20:48:28 -0700

    Add navbar

--- templates/main-portfolio-page.php
+++ templates/main-portfolio-page.php
@@ -18,30 +18,42 @@ remove_action('wp_head', '_admin_bar_bump_cb');
     console_log("portfolioCSS was NOT enqueued");
   }
 ?>
-<body>
-  <header>
-  <nav class="navbar navbar-expand-lg">
-  <div class="container-fluid">
-    <p><a class="navbar-brand" href="#">AN</a></p>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#portfolioNavbar" aria-controls="portfolioNavbar" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="portfolioNavbar">
-      <ul class="navbar-nav ms-auto">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">About</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Contact</a>
-        </li>
-      </ul>
+<body class="portfolio-body">
+  <nav class="navbar navbar-expand navbar-dark portfolio-navbar">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">
+        <img src="<?php echo get_stylesheet_directory_uri();?>/assets/images/portfolio-images/nav-logo.png" alt="" width="30" height="24" class="d-inline-block align-text-top">
+      </a>
+      <div id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link portfolio-navlink active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link portfolio-navlink" href="#">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link portfolio-navlink" href="#">Contact</a>
+          </li>
+        </ul>
+      </div>
     </div>
-  </div>
   </nav>
+  <header>
+    
   </header>
+  <main class="container-fluid">
+    <div class="row">
+      <div class="col-xs-12">
+        <div class="card portfolio-card">
+          <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+        </div>
+      </div>
+    </div>
+  </main>
 </body>
 
 <?php 
