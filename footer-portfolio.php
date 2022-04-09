9938758750c37394eefe33d6dbc4cb4946543c0b
= <anguyen031@gmail.com>
2022-04-08 21:30:39 -0700

    Add links to cards

--- footer-portfolio.php
+++ footer-portfolio.php
@@ -3,8 +3,8 @@
 <footer class="container portfolio-footer mt-5 py-5"> 
   <nav class="row">
     <div class="offset-3 col-6 d-flex justify-content-around">
-      <i class="fab fa-github footer-icon"></i>
-      <i class="fab fa-linkedin footer-icon"></i>
+      <a href="https://github.com/FreudCat"><i class="fab fa-github footer-icon"></i></a>
+      <a href="https://www.linkedin.com/in/AngelaNguyenDVM"><i class="fab fa-linkedin footer-icon"></i></a>
     </div>
     <div class="col-12 d-flex justify-content-evenly mt-3">
       <a class="nav-link portfolio-navlink" href="#">About</a>
--- templates/main-portfolio-page.php
+++ templates/main-portfolio-page.php
@@ -44,52 +44,61 @@ remove_action('wp_head', '_admin_bar_bump_cb');
   </header>
   <main class="container">
     <div class="row gy-5">
-      <div class="col-xs-12 d-flex justify-content-center">
-        <div class="card border-2 portfolio-card">
-          <img src="<?php echo get_stylesheet_directory_uri();?>/assets/images/landing-page-images/pets.jpg" class="card-img-top" alt="Two dogs and two cats sitting together in front of pink background">
-          <div class="card-body">
-            <p class="card-text portfolio-card-text">An uncomplicated page showcasing price packages for various activities.</p>
+      <a class="card-link-text" href="<? echo home_url('/pets-landing-page-template')?>" target="_blank">
+        <div class="col-xs-12 d-flex justify-content-center">
+          <div class="card border-2 portfolio-card">
+            <img src="<?php echo get_stylesheet_directory_uri();?>/assets/images/landing-page-images/pets.jpg" class="card-img-top" alt="Two dogs and two cats sitting together in front of pink background">
+            <div class="card-body">
+              <p class="card-text portfolio-card-text">An uncomplicated page showcasing price packages for various activities.</p>
+            </div>
+            <div class="card-corner"><div class="corner-arrow">→</div></div>
           </div>
-          <div class="card-corner"><div class="corner-arrow">→</div></div>
-          
         </div>
-      </div>
-      <div class="col-xs-12 d-flex justify-content-center">
-        <div class="card portfolio-card border-2">
-          <img src="<?php echo get_stylesheet_directory_uri();?>/assets/images/portfolio-images/ecommerce.PNG" class="card-img-top portfolio-card-image" alt="Sideview of tan, brown, and orange sneaker with flat tread placed onto of two flat rocks">
-          <div class="card-body">
-            <p class="card-text portfolio-card-text">An example of an e-commerce site shopping site. Let's buy some shoes!</p>
+      </a>
+      <a class="card-link-text" href="<? echo home_url('/sneaker-company')?>" target="_blank">
+        <div class="col-xs-12 d-flex justify-content-center">
+          <div class="card portfolio-card border-2">
+            <img src="<?php echo get_stylesheet_directory_uri();?>/assets/images/portfolio-images/ecommerce.PNG" class="card-img-top portfolio-card-image" alt="Sideview of tan, brown, and orange sneaker with flat tread placed onto of two flat rocks">
+            <div class="card-body">
+              <p class="card-text portfolio-card-text">An example of an e-commerce site shopping site. Let's buy some shoes!</p>
+            </div>
+            <div class="card-corner"><div class="corner-arrow">→</div></div>
           </div>
-          <div class="card-corner"><div class="corner-arrow">→</div></div>
         </div>
-      </div>
-      <div class="col-xs-12 d-flex justify-content-center">
-        <div class="card border-2 portfolio-card">
-          <img src="<?php echo get_stylesheet_directory_uri();?>/assets/images/portfolio-images/chicago.jpg" class="card-img-top portfolio-card-image" alt="Chicago marquee and skyline">
-          <div class="card-body">
-            <p class="card-text portfolio-card-text">A gallery showing some of my favorite paintings, operas, and artists.</p>
+      </a>
+      <a class="card-link-text" href="<? echo home_url('/photo-gallery')?>" target="_blank">
+        <div class="col-xs-12 d-flex justify-content-center">
+          <div class="card border-2 portfolio-card">
+            <img src="<?php echo get_stylesheet_directory_uri();?>/assets/images/portfolio-images/chicago.jpg" class="card-img-top portfolio-card-image" alt="Chicago marquee and skyline">
+            <div class="card-body">
+              <p class="card-text portfolio-card-text">A gallery showing some of my favorite paintings, operas, and artists.</p>
+            </div>
+            <div class="card-corner"><div class="corner-arrow">→</div></div>
           </div>
-          <div class="card-corner"><div class="corner-arrow">→</div></div>
         </div>
-      </div>
-      <div class="col-xs-12 d-flex justify-content-center">
-        <div class="card border-2 portfolio-card">
-          <img src="<?php echo get_stylesheet_directory_uri();?>/assets/images/sunny-page-images/transform.jpg" class="card-img-top portfolio-card-image" alt="Two dogs and two cats sitting together in front of pink background">
-          <div class="card-body">
-            <p class="card-text portfolio-card-text">Light and cheery landing page to brighten up your day.</p>
+      </a>
+      <a class="card-link-text" href="<? echo home_url('/sunny-landing-page')?>" target="_blank">
+        <div class="col-xs-12 d-flex justify-content-center">
+          <div class="card border-2 portfolio-card">
+            <img src="<?php echo get_stylesheet_directory_uri();?>/assets/images/sunny-page-images/transform.jpg" class="card-img-top portfolio-card-image" alt="Two dogs and two cats sitting together in front of pink background">
+            <div class="card-body">
+              <p class="card-text portfolio-card-text">Light and cheery landing page to brighten up your day.</p>
+            </div>
+            <div class="card-corner"><div class="corner-arrow">→</div></div>
           </div>
-          <div class="card-corner"><div class="corner-arrow">→</div></div>
         </div>
-      </div>
-      <div class="col-xs-12 d-flex justify-content-center">
-        <div class="card border-2 portfolio-card">
-          <img src="<?php echo get_stylesheet_directory_uri();?>/assets/images/swervy-page-images/illustration-your-users.svg" class="card-img-top portfolio-card-image" alt="Two dogs and two cats sitting together in front of pink background">
-          <div class="card-body">
-            <p class="card-text portfolio-card-text">Another landing page example, but this one is...curvy.</p>
+      </a>
+      <a class="card-link-text" href="<? echo home_url('/swervy-landing-page')?>" target="_blank">
+        <div class="col-xs-12 d-flex justify-content-center">
+          <div class="card border-2 portfolio-card">
+            <img src="<?php echo get_stylesheet_directory_uri();?>/assets/images/swervy-page-images/illustration-your-users.svg" class="card-img-top portfolio-card-image" alt="Two dogs and two cats sitting together in front of pink background">
+            <div class="card-body">
+              <p class="card-text portfolio-card-text">Another landing page example, but this one is...curvy.</p>
+            </div>
+            <div class="card-corner"><div class="corner-arrow">→</div></div>
           </div>
-          <div class="card-corner"><div class="corner-arrow">→</div></div>
         </div>
-      </div>
+      </a>
     </div>
   </main>
   <?php 
