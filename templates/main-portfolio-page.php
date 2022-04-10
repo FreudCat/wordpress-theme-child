2623641f8627e403aeff7ef2c31a037fbd90e5a9
= <anguyen031@gmail.com>
2022-04-10 12:38:09 -0700

    Complete portfolio page

--- templates/main-portfolio-page.php
+++ templates/main-portfolio-page.php
@@ -22,15 +22,15 @@ remove_action('wp_head', '_admin_bar_bump_cb');
   <nav class="navbar navbar-expand navbar-dark pb-0 portfolio-navbar">
     <div class="container-fluid d-flex align-items-end">
       <a class="navbar-brand" href="#">
-        <img src="<?php echo get_stylesheet_directory_uri();?>/assets/images/portfolio-images/nav-logo.png" alt="logo" width="30" height="24" class="d-inline-block align-text-top">
+        <img src="<?php echo get_stylesheet_directory_uri();?>/assets/images/portfolio-images/nav-logo.png" alt="logo" class="d-inline-block align-text-top logo-image">
       </a>
       <div id="navbarNav">
-        <ul class="navbar-nav">
+        <ul class="navbar-nav me-mid-lg-5">
           <li class="nav-item">
-            <a class="nav-link pb-3 portfolio-navlink active" aria-current="page" href="#">Home</a>
+            <a class="nav-link pb-3 pb-mid-lg-4 portfolio-navlink active" aria-current="page" href="#">Home</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link pb-3 portfolio-navlink" href="<? echo home_url('/about')?>">About</a>
+            <a class="nav-link pb-3 pb-mid-lg-4 portfolio-navlink" href="<? echo home_url('/about')?>">About</a>
           </li>
         </ul>
       </div>
@@ -41,11 +41,11 @@ remove_action('wp_head', '_admin_bar_bump_cb');
   </header>
   <main class="container">
     <div class="row portfolio-top-row">
-      <a class="card-link-text col-xs-12 col-md-6 d-flex justify-content-md-evenly" href="<? echo home_url('/pets-landing-page-template')?>" target="_blank">
+      <a class="card-link-text col-xs-12 col-md-6" href="<? echo home_url('/pets-landing-page-template')?>" target="_blank">
         <div class="mb-5 d-flex justify-content-center justify-content-md-end">
           <div class="card border-2 portfolio-card">
             <img src="<?php echo get_stylesheet_directory_uri();?>/assets/images/landing-page-images/pets.jpg" class="card-img-top portfolio-card-image" alt="Two dogs and two cats sitting together in front of pink background">
-            <div class="card-body">
+            <div class="card-body portfolio-card-body">
               <p class="card-text portfolio-card-text">An uncomplicated page showcasing price packages for various activities.</p>
             </div>
             <div class="card-corner"><div class="corner-arrow">→</div></div>
@@ -56,7 +56,7 @@ remove_action('wp_head', '_admin_bar_bump_cb');
         <div class="mb-5 d-flex justify-content-center justify-content-md-start">
           <div class="card portfolio-card border-2">
             <img src="<?php echo get_stylesheet_directory_uri();?>/assets/images/portfolio-images/ecommerce.PNG" class="card-img-top portfolio-card-image" alt="Sideview of tan, brown, and orange sneaker with flat tread placed onto of two flat rocks">
-            <div class="card-body">
+            <div class="card-body portfolio-card-body">
               <p class="card-text portfolio-card-text">An example of an e-commerce site shopping site. Let's buy some shoes!</p>
             </div>
             <div class="card-corner"><div class="corner-arrow">→</div></div>
@@ -69,7 +69,7 @@ remove_action('wp_head', '_admin_bar_bump_cb');
           <div class=" mb-5 d-flex justify-content-center justify-content-md-end">
             <div class="card border-2 portfolio-card">
               <img src="<?php echo get_stylesheet_directory_uri();?>/assets/images/portfolio-images/chicago.jpg" class="card-img-top portfolio-card-image" alt="Chicago marquee and skyline">
-              <div class="card-body">
+              <div class="card-body portfolio-card-body">
                 <p class="card-text portfolio-card-text">A gallery showing some of my favorite paintings, operas, and artists.</p>
               </div>
               <div class="card-corner"><div class="corner-arrow">→</div></div>
@@ -80,7 +80,7 @@ remove_action('wp_head', '_admin_bar_bump_cb');
           <div class="mb-5 d-flex justify-content-center justify-content-md-start">
             <div class="card border-2 portfolio-card">
               <img src="<?php echo get_stylesheet_directory_uri();?>/assets/images/sunny-page-images/transform.jpg" class="card-img-top portfolio-card-image" alt="Two dogs and two cats sitting together in front of pink background">
-              <div class="card-body">
+              <div class="card-body portfolio-card-body">
                 <p class="card-text portfolio-card-text">Light and cheery landing page to brighten up your day.</p>
               </div>
               <div class="card-corner"><div class="corner-arrow">→</div></div>
@@ -93,7 +93,7 @@ remove_action('wp_head', '_admin_bar_bump_cb');
           <div class="mb-5 d-flex justify-content-center">
             <div class="card border-2 portfolio-card">
               <img src="<?php echo get_stylesheet_directory_uri();?>/assets/images/swervy-page-images/illustration-your-users.svg" class="card-img-top portfolio-card-image" alt="Two dogs and two cats sitting together in front of pink background">
-              <div class="card-body">
+              <div class="card-body portfolio-card-body">
                 <p class="card-text portfolio-card-text">Another landing page example, but this one is...curvy.</p>
               </div>
               <div class="card-corner"><div class="corner-arrow">→</div></div>
