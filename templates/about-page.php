0f7e2b37cec40645a8da7edf9630b1a3f6791a19
= <anguyen031@gmail.com>
2022-04-11 20:41:29 -0700

    Add text to About page

--- templates/about-page.php
+++ templates/about-page.php
@@ -9,18 +9,20 @@ remove_action('wp_head', '_admin_bar_bump_cb');
   <?php get_template_part( 'template-parts/portfolio-nav' );?>
   <header class="container mb-5 portfolio-header">
     <div class="row">
-      <div class="col-2 col-8 mt-2 about-image-holder">
-        <h1 class="about-header-text">What is this all about?</h1>
+      <div class="col-2 col-8 mt-5 mb-5 about-image-holder">
+        <h1 class="about-header-text">About</h1>
         <img class="about-image" src="<?php echo get_stylesheet_directory_uri();?>/assets/images/portfolio-images/cherry-blossom.jpg" alt="Close up of cherry blossom">
       </div>
     </div>
   </header>
   <main class="container">
     <div class="row">
-      <div class="col-12">
-        
+      <div class="offset-1 col-10 offset-md-3 col-md-6">
+        <p class="text-center mt-5 pt-md-5 mb-md-5 about-body-text">Just my little corner of the web showing my landing page exercises. The focus was on establishing foundational concepts, so each page has an abstract explaining tools I used and things I learned.</p>
       </div>
-      <div class="col-12"></div>
+      <div class="offset-3 col-6 offset-md-4 col-md-4">
+        <h2 class="text-center mt-5 pt-md-5 about-bold-text">Excited to learn more!</h2>
+      </div>
     </div>
   </main>
   <?php 
