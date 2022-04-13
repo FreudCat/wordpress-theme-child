6d05a51e64013a23a861de9057b20bcd5e70b4a2
= <anguyen031@gmail.com>
2022-04-12 20:35:32 -0700

    Add styles for about page md viewpoint

--- templates/about-page.php
+++ templates/about-page.php
@@ -7,8 +7,8 @@ remove_action('wp_head', '_admin_bar_bump_cb');
 
 <body class="portfolio-body">
   <?php get_template_part( 'template-parts/portfolio-nav' );?>
-  <header class="container mb-5 portfolio-header">
-    <div class="row">
+  <header class="container mb-5 d-md-flex justify-content-md-center about-header">
+    <div class="row about-row ">
       <div class="col-2 col-8 mt-5 mb-5 about-image-holder">
         <h1 class="about-header-text">About</h1>
         <img class="about-image" src="<?php echo get_stylesheet_directory_uri();?>/assets/images/portfolio-images/cherry-blossom.jpg" alt="Close up of cherry blossom">
@@ -16,7 +16,7 @@ remove_action('wp_head', '_admin_bar_bump_cb');
     </div>
   </header>
   <main class="container">
-    <div class="row">
+    <div class="row about-content-row">
       <div class="offset-1 col-10 offset-md-3 col-md-6">
         <p class="text-center mt-5 pt-md-5 mb-md-5 about-body-text">Just my little corner of the web showing my landing page exercises. The focus was on establishing foundational concepts, so each page has an abstract explaining tools I used and things I learned.</p>
       </div>
